c05dfe935082cebfe9ab10700622b7aad6828126
paul <piankin250@gmail.com>
2020-10-27 00:30:12 +0300

    Wait class

---
+++ Routins/Demo.cs
@@ -0,0 +1,18 @@
+﻿using OneMoreCSharpFramework.src.driver;
+using OneMoreCSharpFramework.src.pages;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneMoreCSharpFramework.routins
+{
+    public class Demo 
+    {
+        public void MyTest()
+        {
+            PentaPage p = new PentaPage();
+            p.Search();
+        }
+    }
+}
---
+++ Source/Pages/BasePage.cs
@@ -0,0 +1,21 @@
+﻿using OneMoreCSharpFramework.Source.Driver.Waits;
+using OneMoreCSharpFramework.src.driver;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace OneMoreCSharpFramework.src.pages
+{
+    public abstract class BasePage : DriverClass
+    {
+        public IWebDriver driver;
+        public WaitMethods waitMethods;
+
+        public BasePage() 
+        {
+            this.driver = DriverClass.GetDriver();
+            PageFactory.InitElements(DriverClass.GetDriver(), this);
+        }
+
+        public WaitMethods WaitMethods => waitMethods ?? new WaitMethods(driver);
+    }
+}
