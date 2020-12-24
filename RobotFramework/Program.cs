1749142bf2a25f5ed8f1a9c778fa43279fe76252
paul <piankin250@gmail.com>
2020-12-25 00:28:06 +0300

    FrameworkRobot added

---
+++ RobotFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RobotFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
