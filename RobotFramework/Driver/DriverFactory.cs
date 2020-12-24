72c0879d1148aee3582c2675cd647cd25ba8c800
paul <piankin250@gmail.com>
2020-12-25 01:48:48 +0300

    Driver class

---
+++ RobotFramework/Driver/DriverFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RobotFramework.Driver
+{
+    class DriverFactory
+    {
+        public void CreateDriver()
+        {
+
+        }
+    }
+}
