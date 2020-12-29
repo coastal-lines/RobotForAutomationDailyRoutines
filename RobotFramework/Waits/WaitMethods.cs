10ea6409882fdb8403b97754a7fe2077a9ae83e9
paul <piankin250@gmail.com>
2020-12-29 14:12:42 +0300

    Wait

--- RobotFramework/Waits/WaitMethods.cs
+++ RobotFramework/Waits/WaitMethods.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using RobotFramework.Driver;
+using SeleniumExtras.WaitHelpers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace RobotFramework.Waits
 {
-    class WaitMethods
+    public class WaitMethods : DriverClass
     {
+        private IWebDriver driver;
+        private const int ExplicitWaitingTime = 10;
+
+        public WaitMethods(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void WaitElement(IWebElement element)
+        {
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(ExplicitWaitingTime));
+            wait.Until(d => element.Displayed);
+        }
     }
 }
