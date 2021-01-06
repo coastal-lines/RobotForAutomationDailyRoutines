6de744500a22a5f0e9ca294ba9af3d58bede850c
paul <piankin250@gmail.com>
2021-01-06 23:55:59 +0300

    no message

---
+++ RobotFramework/Driver/DriverClass.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace RobotFramework.Driver
+{
+    public class DriverClass : DriverFactory
+    {
+
+        //TODO
+        //This class probably should be for Driver Parameters and ETC
+
+        private IWebDriver driver;
+
+        public void TeadDownDriver()
+        {
+            driver.Close();
+            driver.Quit();
+            driver.Dispose();
+
+            //TODO
+            //Linux?????
+            Process[] chromeDriverProcesses = Process.GetProcessesByName("chromedriver");
+            foreach (var chromeDriverProcess in chromeDriverProcesses)
+            {
+                chromeDriverProcess.Kill();
+            }
+
+            //TODO
+            //Linux?????
+            Process dotnetProcesses = Process.GetProcessesByName("dotnet")[0];
+            dotnetProcesses.Kill();
+
+            //TODO
+            //add firefox driver
+        }
+    }
+}
---
+++ RobotFramework/Driver/DriverFactory.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System.Configuration;
+
+namespace RobotFramework.Driver
+{
+    public class DriverFactory
+    {
+        public IWebDriver CreateDriver(IWebDriver driver, Browser browser = Browser.Chrome)
+        {
+            switch (browser)
+            {
+                case Browser.Chrome:
+                    driver = new ChromeDriver(ConfigurationManager.AppSettings["PathDriver"]);
+                    //driver = new ChromeDriver("/home/user/Desktop/Project/Driver/");
+                    break;
+
+                case Browser.Firefox:
+                    driver = new FirefoxDriver();
+                    break;
+            }
+
+            return driver;
+        }
+    }
+}
